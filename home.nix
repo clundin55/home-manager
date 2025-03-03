d338c03950c6e9148ccb39c2307d97088d59ef1e
Carl Lundin <clundin@google.com>
2025-03-03 10:15:18 -0800

    Sync home.nix with clundin55/nixos.

--- home.nix
+++ home.nix
@@ -5,13 +5,29 @@
   home.homeDirectory = "/home/carl";
   home.language.base = "en_US.UTF-8";
 
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    plugins = [
+      {
+        # will source zsh-autosuggestions.plugin.zsh
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.7.1";
+          sha256 = "vpTyYq9ZgfgdDsWzjxVAE7FZH4MALMNZIFyEOBLm5Qo=";
+        };
+      }
+    ];
+  };
   home.shell.enableZshIntegration = true;
 
   home.packages = [
     pkgs.alacritty
     pkgs.zsh
-    pkgs.tmux
+    pkgs.clang
     pkgs.neovim
     pkgs.ripgrep
     pkgs.fzf
@@ -22,19 +38,23 @@
     pkgs.difftastic # binary is difft
     pkgs.tealdeer
     pkgs.pass
+    pkgs.gnupg
+    pkgs.pinentry-tty
     pkgs.tree
     pkgs.scmpuff
+    pkgs.go
+    pkgs.gopls
   ];
 
   home.file = {
     ".zshrc".source = dotfiles/zshrc;
     ".inputrc".source = dotfiles/inputrc;
-    ".tmux.conf".source = dotfiles/tmuxconf;
     ".config/nvim/init.lua".source = dotfiles/nvimlua;
     ".config/alacritty/alacritty.toml".source = dotfiles/alacrittytoml;
     ".config/alacritty/darcula.toml".source = dotfiles/darculatoml;
     ".config/alacritty/paper.toml".source = dotfiles/papertoml;
     ".config/hypr/hyprland.conf".source = dotfiles/hyprlandconf;
+    ".config/hypr/hyprlock.conf".source = dotfiles/hyprlockconf;
     ".config/hypr/hyprpaper.conf".source = dotfiles/hyprpaperconf;
     ".config/waybar/config".source = dotfiles/waybarconf;
     ".config/waybar/style.css".source = dotfiles/waybarstyle;
@@ -62,6 +82,64 @@
       "st" = "stash";
       "ps" = "push";
     };
+  };
+
+  programs.tmux = {
+    enable = true;
+    shell = "${pkgs.zsh}/bin/zsh";
+    baseIndex = 1;
+    mouse = true;
+    plugins = [
+      {
+        plugin = pkgs.tmuxPlugins.dracula;
+        extraConfig = ''
+            set -g @dracula-plugins "cpu-usage ram-usage"
+            set -g @dracula-show-powerline true
+        '';
+      }
+      {
+        plugin = pkgs.tmuxPlugins.sensible;
+      }
+      {
+        plugin = pkgs.tmuxPlugins.continuum;
+        extraConfig = ''
+            set -g @continuum-restore 'on'
+            set -g @continuum-save-interval '60'
+        '';
+      }
+      {
+        plugin = pkgs.tmuxPlugins.resurrect;
+        extraConfig = ''
+            set -g @resurrect-strategy-vim 'session'
+            set -g @resurrect-strategy-nvim 'session'
+            set -g @resurrect-capture-pane-contents 'on'
+        '';
+      }
+    ];
+    extraConfig = ''
+        bind h select-pane -L
+        bind j select-pane -D
+        bind k select-pane -U
+        bind l select-pane -R
+        set -g default-terminal "screen-256color"
+        bind c new-window -c "#{pane_current_path}"
+        bind '"' split-window -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+        setw -g mode-keys vi
+        bind-key -T copy-mode-vi 'v' send -X begin-selection
+        bind-key -T copy-mode-vi 'C-v' send -X rectangle-toggle
+        bind-key -T copy-mode-vi 'y' send -X copy-selection-no-clear
+        bind-key -n c-a send-prefix
+        set -g set-clipboard on 
+        set-option -g allow-rename off
+    '';
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+    pinentryPackage = pkgs.pinentry-tty;
   };
 
   programs.home-manager.enable = true;
